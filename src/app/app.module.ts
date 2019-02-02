5f207bd33451d98a4503a384f0d9945f29f6def7
Rashadtheone <rashad.hammonds0114@gmail.com>
2019-02-02 13:56:15 -0500

    outline.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { TableRowComponent } from './table-row/table-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    TableRowComponent
   ],
   imports: [
     BrowserModule
